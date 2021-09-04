81649cd665347f7dc986326bdf2d81253b377ccd
Thomas <thomas.bergamin@placemeet.fr>
2021-09-04 17:16:47 +0200

    import token for JWToken

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const User = require("../models/User");
 
@@ -30,7 +32,7 @@ exports.login = (req, res, next) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+            token: jwt.sign({ userId: user._id }, process.env.SECRET_TOKEN, {
               expiresIn: "24h",
             }),
           });
