877b4e7cf612f9d333c669f384dcff80071a363a
Thomas <thomas.bergamin@placemeet.fr>
2021-09-04 17:46:30 +0200

    add missing import

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 
 const userRoutes = require("./routes/user");
