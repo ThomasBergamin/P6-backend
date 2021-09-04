3f29b57413c7a2b5a8c1f298d6c8f02a884369b4
Thomas <thomas.bergamin@placemeet.fr>
2021-09-04 17:46:04 +0200

    add static route

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use("/api/auth", userRoutes);
 
 module.exports = app;
