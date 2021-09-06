09a7587bebccfa2e9b36b400fed16fa91cc42104
Thomas <thomas.bergamin@placemeet.fr>
2021-09-06 22:51:49 +0200

    init sauce controllers

---
+++ controllers/sauce.js
@@ -0,0 +1,13 @@
+const Sauce = require("../models/Sauce");
+
+exports.createSauce = (req, res, next) => {};
+
+exports.getOneSauce = (req, res, next) => {};
+
+exports.modifySauce = (req, res, next) => {};
+
+exports.deleteSauce = (req, res, next) => {};
+
+exports.getAllSauces = (req, res, next) => {};
+
+exports.addLike = (req, res, next) => {};
