3da4a5b8802ce25015308a4d1652e15ece8f1c1a
Thomas <thomas.bergamin@placemeet.fr>
2021-09-06 23:20:21 +0200

    add create Sauce route

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -12,7 +12,25 @@ exports.getAllSauces = (req, res, next) => {
     });
 };
 
-exports.createSauce = (req, res, next) => {};
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  console.log(sauceObject);
+  console.log(sauceObject.manufacturer);
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({
